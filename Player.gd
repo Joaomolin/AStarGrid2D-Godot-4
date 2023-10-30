a7bebe686798c61e58670d8e1a459d6267fd4d9c
MrJooj <54430308+MrJooj@users.noreply.github.com>
2023-10-30 20:41:51 -0300

    Add comments

--- Player.gd
+++ Player.gd
@@ -10,8 +10,7 @@ var grid: AStarGrid2D
 var idPath:PackedVector2Array
 var otherIdPath:PackedVector2Array
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _ready():
 	#Start grid
 	grid = AStarGrid2D.new()
 	grid.region = tileMap.get_used_rect()
@@ -21,7 +20,6 @@ func _ready() -> void:
 
 func _process(_delta):
 	queue_redraw()
-	pass
 
 func _input(event: InputEvent) -> void:
 	if !event.is_action_pressed("ui_accept"):	return
@@ -31,24 +29,22 @@ func _input(event: InputEvent) -> void:
 	idPath = grid.get_id_path(_from, _to)
 	
 	otherIdPath = grid.get_point_path(_from, _to)
-	for i in otherIdPath.size():		
+	for i in otherIdPath.size():
 		otherIdPath[i] -= global_position #Fix start path position to center
 		otherIdPath[i] += Vector2(tileSize.x / 2, tileSize.y / 2) #Push vector pos to center of character
 
 func _draw():
-	if otherIdPath.size() > 1:
-		#Draw path line
-		draw_polyline(otherIdPath, Color.RED)
+	if otherIdPath.size() > 1:#If has path to walk
+		draw_polyline(otherIdPath, Color.RED)#Draw path line
 		
 		#Draw circles on center of path tiles
 		for i in otherIdPath.size():
 			draw_circle(otherIdPath[i], 1, Color.RED)
-			
-	#Draw squares on tiles
-	print(tileMap.tile_set.tile_size)
+
 	if tileMap:
+		#Draw squares on tiles
 		var arrayOfCells = tileMap.get_used_cells(0)
 		for i in arrayOfCells.size():
-			var _cellX = arrayOfCells[i].x * tileSize.x - 160 # -160 to Fix vectors pos to upper left corner of screen
-			var _cellY = arrayOfCells[i].y * tileSize.y - 160 # -160 to Fix vectors pos to upper left corner of screen
+			var _cellX = arrayOfCells[i].x * tileSize.x - get_transform().origin.x # Scale the cells based on tile size, and fix vectors pos to upper left corner of screen
+			var _cellY = arrayOfCells[i].y * tileSize.y - get_transform().origin.y # to Fix vectors pos to upper left corner of screen
 			draw_rect(Rect2(_cellX, _cellY, tileSize.x, tileSize.y), Color.RED, false) 
